99775e6bf50d54fad6b5b01933f3f933b1e79af1
CJ Mejia <cjconradjoshmejia@gmail.com>
2026-06-27 16:05:53 -0400

    added nights edge brick variant that shoots projectiles

--- Content/Items/Weapons/Melee/NightsBrick.cs
+++ Content/Items/Weapons/Melee/NightsBrick.cs
@@ -1,12 +1,103 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using SomethingCreative.Content.Projectiles;
+using System.Security.Cryptography.Xml;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace SomethingCreative.Content.Items.Weapons.Melee
 {
-    internal class NightsBrick
+    public class NightsBrick : ModItem
     {
+        public override void SetDefaults()
+        {
+            Item.Size = new Vector2(32, 32);
+            Item.DamageType = DamageClass.Melee;
+            Item.damage = 25;
+
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.UseSound = SoundID.Item1;
+            Item.knockBack = 7;
+
+            Item.useAnimation = 35;
+            Item.useTime = 35;
+
+            Item.scale = 2.25f;
+            Item.crit = -15;
+            Item.rare = ItemRarityID.Purple;
+            Item.value = 67;
+            Item.useTurn = true;
+            Item.shootSpeed = 30f;
+            Item.shoot = ModContent.ProjectileType<NightsBrickProjectile>();
+
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().AddIngredient(ModContent.ItemType<EnchantedBrick>(), 1).AddIngredient(ItemID.DemoniteBar, 15).AddIngredient(ItemID.ShadowScale, 10).AddTile(TileID.DemonAltar).Register();
+            CreateRecipe().AddIngredient(ModContent.ItemType<EnchantedBrick>(), 1).AddIngredient(ItemID.CrimtaneBar, 15).AddIngredient(ItemID.TissueSample, 10).AddTile(TileID.DemonAltar).Register();
+        }
+
+        public override void UseStyle(Player player, Rectangle heldItemFrame)
+        {
+            Vector2 direction = Main.MouseWorld - player.Center;
+            //player.itemRotation = direction.ToRotation();
+
+            //flip player based on mouse direction
+            player.direction = direction.X >= 0 ? 1 : -1;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source,
+           Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Vector2 direction = (Main.MouseWorld - player.Center).SafeNormalize(Vector2.UnitX);
+            velocity = direction * Item.shootSpeed;
+
+            Projectile.NewProjectile(source, player.Center, velocity, type, damage, knockback, player.whoAmI);
+            SoundEngine.PlaySound(SoundID.DD2_BetsysWrathShot, player.Center);
+
+            
+            return false;
+        }
+
+
+
+
+
+        public override void OnHitNPC(Player player, NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (hit.Crit)
+            {
+
+                int extraDamage = damageDone * 9;
+                target.SimpleStrikeNPC(extraDamage, 0, false, 0, DamageClass.Melee, false, 0, false);
+                SoundEngine.PlaySound(SoundID.Dig, target.Center);
+                for (int i = 0; i < 20; i++)
+                {
+                    Dust d = Dust.NewDustPerfect(
+                        target.Center,
+                        DustID.Wraith,
+                        Main.rand.NextVector2Circular(10f, 10f),
+                        150,
+                        default,
+                        1.4f
+                    );
+                    Dust d2 = Dust.NewDustPerfect(
+                        target.Center,
+                        DustID.GemAmethyst,
+                        Main.rand.NextVector2Circular(10f, 10f),
+                        150,
+                        default,
+                        1.4f
+                    );
+                    d2.noGravity = true;
+                    d.noGravity = true;
+                }
+            }
+        }
+
+
     }
 }
--- Content/Projectiles/NightsBrickProjectile.cs
+++ Content/Projectiles/NightsBrickProjectile.cs
@@ -9,85 +9,57 @@ using Terraria.ModLoader;
 
 namespace SomethingCreative.Content.Projectiles
 {
-    public class EnchantedBrickProjectile : ModProjectile
+    public class NightsBrickProjectile : ModProjectile
     {
         public override void SetDefaults()
         {
-            Projectile.width = 24;
-            Projectile.height = 24;
+            Projectile.width = 30;
+            Projectile.height = 30;
 
-            Projectile.scale = 1.5f;
+            Projectile.scale = 4f;
 
             Projectile.friendly = true;
             Projectile.hostile = false;
             Projectile.tileCollide = false;
 
-            Projectile.penetrate = 3;
-            Projectile.timeLeft = 90;
-            Projectile.alpha = 0;
+            Projectile.penetrate = 100;
+            Projectile.timeLeft = 30;
+            Projectile.alpha = 175;
 
 
             Projectile.DamageType = DamageClass.Melee;
         }
+
 
         public override void AI()
         {
             //Main.NewText("Alpha: " + Projectile.alpha);
 
             Projectile.ai[0]++;
-
-            Projectile.velocity *= 0.98f;
-            Projectile.rotation += 0.5f;
+            Projectile.rotation = Projectile.velocity.ToRotation();
+            Projectile.velocity *= 0.97f;
             Lighting.AddLight(Projectile.Center, 0.3f, 0.3f, 1f);
 
-            if (Projectile.ai[0] < 30)
-            {
-                Dust d = Dust.NewDustPerfect(
-                Projectile.Center,
-                DustID.MagicMirror,
-                Projectile.velocity * -0.2f,
-                150,
-                default,
-                1.2f
+            Dust d = Dust.NewDustPerfect(
+             Projectile.Center,
+             DustID.Wraith,
+             Main.rand.NextVector2Circular(10f, 10f),
+             150,
+             default,
+             1.4f
 
-             );
-                d.noGravity = true;
-            }
+          );
+            Dust d2 = Dust.NewDustPerfect(
+             Projectile.Center,
+             DustID.GemAmethyst,
+             Main.rand.NextVector2Circular(10f, 10f),
+             150,
+             default,
+             1.4f
 
-            if (Projectile.ai[0] > 30)
-            {
-                Projectile.alpha += 255/60;
-                if (Projectile.alpha > 255)
-                    Projectile.alpha = 255;
-            }
-
-
-
-
-
-
-
-
-            if (Projectile.ai[0] == 30)
-            {
-                Projectile.velocity = Vector2.Zero;
-                Projectile.penetrate = 10;
-
-                for (int i = 0; i < 20; i++)
-                {
-                    Dust d2 = Dust.NewDustPerfect(
-                        Projectile.Center,
-                        DustID.MagicMirror,
-                        Main.rand.NextVector2Circular(10f, 10f),
-                        150,
-                        default,
-                        1.4f
-                    );
-                    d2.noGravity = true;
-                }
-
-                SoundEngine.PlaySound(SoundID.Item10, Projectile.Center);
-            }
+          );
+            d.noGravity = true;
+            d2.noGravity = true;
 
         }
         public override bool PreDraw(ref Color lightColor)
@@ -96,17 +68,17 @@ namespace SomethingCreative.Content.Projectiles
 
             Vector2 origin = new Vector2(texture.Width / 2f, texture.Height / 2f);
 
-            
+
             float alphaMult = (255 - Projectile.alpha) / 255f;
 
-           
+
             Color drawColor = lightColor * alphaMult;
 
             Main.EntitySpriteDraw(
                 texture,
                 Projectile.Center - Main.screenPosition,
                 null,
-                drawColor,              
+                drawColor,
                 Projectile.rotation,
                 origin,
                 Projectile.scale,
@@ -122,27 +94,34 @@ namespace SomethingCreative.Content.Projectiles
         {
             if (hit.Crit)
             {
-                int extraDamage = damageDone * 7;
+                int extraDamage = damageDone * 5;
                 target.SimpleStrikeNPC(extraDamage, 0, false, 0, DamageClass.Melee, false, 0, false);
 
                 SoundEngine.PlaySound(SoundID.AbigailAttack, target.Center);
                 for (int i = 0; i < 20; i++)
                 {
+                    Dust d = Dust.NewDustPerfect(
+                    Projectile.Center,
+                    DustID.Wraith,
+                    Main.rand.NextVector2Circular(10f, 10f),
+                    150,
+                    default,
+                    1.4f
+
+                 );
                     Dust d2 = Dust.NewDustPerfect(
-                        target.Center,
-                        DustID.MagicMirror,
-                        Main.rand.NextVector2Circular(10f, 10f),
-                        150,
-                        default,
-                        1.4f
-                    );
+                     Projectile.Center,
+                     DustID.GemAmethyst,
+                     Main.rand.NextVector2Circular(10f, 10f),
+                     150,
+                     default,
+                     1.4f
+
+                 );
+                    d.noGravity = true;
                     d2.noGravity = true;
                 }
             }
         }
-
-
-
     }
-
 }
