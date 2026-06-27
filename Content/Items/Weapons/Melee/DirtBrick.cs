96afafa4668fc39a2d0e32c5c79de80819d5b72a
CJ Mejia <cjconradjoshmejia@gmail.com>
2026-06-26 21:39:40 -0400

    Add project files.

---
+++ Content/Items/Weapons/Melee/DirtBrick.cs
@@ -0,0 +1,48 @@
+﻿using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria;
+
+namespace SomethingCreative.Content.Items.Weapons.Melee
+{
+    public class DirtBrick : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.Size = new Vector2(32, 32);
+            Item.DamageType = DamageClass.Melee;
+            Item.damage = 5;
+
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.UseSound = SoundID.Item1;
+            Item.knockBack = 2;
+
+            Item.useAnimation = 20;
+            Item.useTime = 20;
+
+            Item.scale = 1.5f;
+            Item.crit = -5;
+            Item.rare = ItemRarityID.Green;
+            Item.value = 0;
+
+        }
+
+        public override void AddRecipes() { 
+            CreateRecipe().AddIngredient(ItemID.DirtBlock, 67).AddTile(TileID.WorkBenches).Register();
+        }
+
+
+
+        public override void OnHitNPC(Player player, NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (hit.Crit)
+            {
+                
+                int extraDamage = damageDone * 10;
+                target.SimpleStrikeNPC(extraDamage,0,false,0,DamageClass.Melee,false,0,false);
+            }
+        }
+
+
+    }
+}
---
+++ Content/Items/Weapons/Melee/EnchantedBrick.cs
@@ -0,0 +1,55 @@
+﻿using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria;
+
+namespace SomethingCreative.Content.Items.Weapons.Melee
+{
+    public class EnchantedBrick : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.Size = new Vector2(32, 32);
+            Item.DamageType = DamageClass.Melee;
+            Item.damage = 10;
+
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.UseSound = SoundID.Item1;
+            Item.knockBack = 2;
+
+            Item.useAnimation = 30;
+            Item.useTime = 30;
+
+            Item.scale = 3f;
+            Item.crit = -10;
+            Item.rare = ItemRarityID.Green;
+            Item.value = 0;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.EnchantedSword, 1);
+            recipe.AddIngredient(ModContent.ItemType<DirtBrick>(), 1);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+
+        }
+
+
+
+
+
+        public override void OnHitNPC(Player player, NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (hit.Crit)
+            {
+
+                int extraDamage = damageDone * 7;
+                target.SimpleStrikeNPC(extraDamage, 0, false, 0, DamageClass.Melee, false, 0, false);
+            }
+        }
+
+
+    }
+}
