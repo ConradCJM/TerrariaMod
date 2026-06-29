b35ab3a718281ece14ecff74c22566c97ca790e9
CJ Mejia <cjconradjoshmejia@gmail.com>
2026-06-29 19:44:07 -0400

    finish creating lego brick. has a calamity mod integrated recipe and a vanilla recipe

--- Content/Items/Weapons/Melee/LegoBrick.cs
+++ Content/Items/Weapons/Melee/LegoBrick.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
-using SomethingCreative.Content.Projectiles;
-using System.Security.Cryptography.Xml;
-using System.Threading.Tasks;
 using Terraria;
 using Terraria.Audio;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
+using System;
 
 namespace SomethingCreative.Content.Items.Weapons.Melee
 {
@@ -14,7 +12,7 @@ namespace SomethingCreative.Content.Items.Weapons.Melee
     {
         public override void SetDefaults()
         {
-            Item.Size = new Vector2(32, 32);
+            Item.Size = new Vector2(40, 25);
             Item.DamageType = DamageClass.Melee;
             Item.damage = 500;
 
@@ -22,41 +20,165 @@ namespace SomethingCreative.Content.Items.Weapons.Melee
             Item.UseSound = SoundID.Item1;
             Item.knockBack = 20;
 
-            Item.useAnimation = 10;
-            Item.useTime = 10;
+            Item.useAnimation = 30;
+            Item.useTime = 6;
 
-            Item.scale = 1f;
+            Item.scale = 1.3f;
             Item.crit = -30;
-            Item.rare = ItemRarityID.Master;
+            if (ModLoader.TryGetMod("CalamityMod", out Mod calamity))
+            {
+                if (calamity.TryFind<ModRarity>("CosmicPurple", out ModRarity cosmicPurple))
+                {
+                    Item.rare = cosmicPurple.Type;
+
+                }
+                else
+                {
+                    Item.rare = ItemRarityID.Master;
+                }
+            }
+            else
+            {
+                Item.rare = ItemRarityID.Master;
+            }
             Item.value = 670000*5;
             Item.shootSpeed = 25f;
+            Item.shoot = ModContent.ProjectileType<Projectiles.LegoProjectile>();
 
         }
 
         public override void AddRecipes()
         {
+            
+            Recipe r = CreateRecipe();
+            r.AddIngredient(ItemID.LunarBar, 25).AddIngredient(ModContent.ItemType<BricksWrath>(), 1);
             //calamity mod recipe
+            if (ModLoader.TryGetMod("CalamityMod", out Mod calamity))
+            {
+                if (calamity.TryFind<ModItem>("CosmiliteBar", out ModItem cosmiliteBar) && calamity.TryFind<ModTile>("CosmicAnvil", out ModTile cosmicAnvil))
+                {
+                    r.AddIngredient(cosmiliteBar.Type, 25).AddTile(cosmicAnvil.Type);
+
+                }
+                else
+                {
+                    r.AddIngredient(ItemID.Cactus);
+                }
+            }
+            else
+            {
+                //no calamity mod recipe
+                r.AddIngredient(ModContent.ItemType<TerraBrick>(), 1).AddIngredient(ModContent.ItemType<NightsBrick>(), 1).AddIngredient(ModContent.ItemType<EnchantedBrick>(), 1).AddIngredient(ModContent.ItemType<DirtBrick>(), 1).AddTile(TileID.LunarCraftingStation);
+            }
+            
 
 
-            //no calamity mod recipe
-            CreateRecipe().AddIngredient(ModContent.ItemType<BricksWrath>(), 1).AddIngredient(ModContent.ItemType<TerraBrick>(), 1).AddIngredient(ModContent.ItemType<NightsBrick>(), 1).AddIngredient(ModContent.ItemType<EnchantedBrick>(), 1).AddIngredient(ModContent.ItemType<DirtBrick>(), 1).AddTile(TileID.LunarCraftingStation).Register();
-
+            r.Register();
+            
+            //works in calamity and base game because of zenith recipe rework in calamity
+            CreateRecipe().AddIngredient(ItemID.Zenith).AddIngredient(ModContent.ItemType<BricksWrath>()).AddTile(TileID.LunarCraftingStation).Register();
         }
 
         public override void UseStyle(Player player, Rectangle heldItemFrame)
         {
             Vector2 direction = Main.MouseWorld - player.Center;
-            //player.itemRotation = direction.ToRotation();
 
             //flip player based on mouse direction
             player.direction = direction.X >= 0 ? 1 : -1;
         }
 
+        
+
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source,
            Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
+            var modPlayer = player.GetModPlayer<LegoBrickPlayer>();
             //Idea: after 12 shots shoots a large projectile that does 10x damage and has a large aoe. towards cursor. projectile explodes into smaller bricks
             //Idea: in a circle that is 200 pixels radius around the cursor, spawn projectiles that move towards the cursor. spawn location is random but on the circles perimeter. spawn 3 projectiles everytime
+
+            //spawn projectiles around cursor here
+            Vector2 cursor = Main.MouseWorld;
+            float radius = 400f;
+
+            for (int i = 0; i < 1; i++)
+            {
+                
+                float angle = Main.rand.NextFloat(MathHelper.TwoPi);
+
+                Vector2 spawnPos = cursor + new Vector2(
+                    (float)System.Math.Cos(angle),
+                    (float)System.Math.Sin(angle)
+                ) * radius;
+                SoundEngine.PlaySound(SoundID.MaxMana with { Volume = 1.0f, Pitch = -0.5f, MaxInstances = 25}, spawnPos);
+                Vector2 vel = (cursor - spawnPos).SafeNormalize(Vector2.Zero) * Item.shootSpeed;
+
+                Projectile.NewProjectile(
+                    source,
+                    spawnPos,
+                    vel,
+                    type,        
+                    damage,
+                    knockback,
+                    player.whoAmI
+                );
+
+                for (int j = 0; j < 10; j++)
+                {
+                    Dust d = Dust.NewDustPerfect(
+                        spawnPos,
+                        DustID.GemRuby,
+                        Main.rand.NextVector2Circular(20f, 20f),
+                        150,
+                        default,
+                        14f
+                    );
+                    Dust d2 = Dust.NewDustPerfect(
+                        spawnPos,
+                        DustID.HeatRay,
+                        Main.rand.NextVector2Circular(20f, 20f),
+                        150,
+                        default,
+                        1.4f
+                    );
+                    Dust d3 = Dust.NewDustPerfect(
+                        spawnPos,
+                        DustID.GemTopaz,
+                        Main.rand.NextVector2Circular(20f, 20f),
+                        150,
+                        default,
+                        1.4f
+                    );
+                    d3.noGravity = true;
+                    d2.noGravity = true;
+                    d.noGravity = true;
+                }
+            }
+
+            if (modPlayer.projectileCooldown > 0 && modPlayer.projectileTimerCooldown > 0)
+            {
+                modPlayer.projectileCooldown -= 1;
+                return false;
+            }
+            SoundEngine.PlaySound(SoundID.DD2_EtherianPortalOpen with { Volume = 2.0f, Pitch = -0.5f, MaxInstances = 12 }, player.Center);
+
+            modPlayer.projectileCooldown = 13*6;
+            modPlayer.projectileTimerCooldown = 60 * 20;
+
+            Vector2 v = (cursor - player.Center).SafeNormalize(Vector2.Zero) * Item.shootSpeed * 0.9f;
+
+            int projectileDamage = Item.damage * 5;
+
+            ModProjectile modProjectile = ModContent.GetModProjectile(ModContent.ProjectileType<Projectiles.BigLegoProjectile>());
+
+            Projectile.NewProjectile(
+                    source,
+                    player.Center,
+                    v,
+                    modProjectile.Type,        
+                    projectileDamage,
+                    knockback,
+                    player.whoAmI
+                );
 
             return false;
         }
@@ -67,6 +189,7 @@ namespace SomethingCreative.Content.Items.Weapons.Melee
 
         public override void OnHitNPC(Player player, NPC target, NPC.HitInfo hit, int damageDone)
         {
+            
             if (hit.Crit)
             {
 
@@ -112,11 +235,13 @@ namespace SomethingCreative.Content.Items.Weapons.Melee
     public class LegoBrickPlayer : ModPlayer
     {
         public int projectileCooldown = 0;
+        public int projectileTimerCooldown = 0;
+
         public override void ResetEffects()
         {
-            if (projectileCooldown > 0)
+            if (projectileTimerCooldown > 0)
             {
-                projectileCooldown--;
+                projectileTimerCooldown--;
             }
         }
     }
---
+++ Content/Projectiles/LegoProjectile.cs
@@ -0,0 +1,221 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SomethingCreative.Content.Projectiles
+{
+    public class LegoProjectile : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.height = 25;
+            Projectile.width = 40;
+            Projectile.scale = 1.5f;
+
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.tileCollide = false;
+
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 240;
+            Projectile.alpha = 0;
+
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+
+            Projectile.DamageType = DamageClass.Melee;
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value;
+
+            Vector2 origin = new Vector2(texture.Width / 2f, texture.Height / 2f);
+
+
+            float alphaMult = (255 - Projectile.alpha) / 255f;
+
+
+            Color drawColor = lightColor * alphaMult;
+
+            Main.EntitySpriteDraw(
+                texture,
+                Projectile.Center - Main.screenPosition,
+                null,
+                drawColor,
+                Projectile.rotation,
+                origin,
+                Projectile.scale,
+                SpriteEffects.None,
+                0
+            );
+
+            return false;
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            
+            if (hit.Crit)
+            {
+                SoundEngine.PlaySound(SoundID.DD2_KoboldExplosion with { Volume = 2f }, Projectile.Center);
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<LegoExplosion>(), Projectile.damage * 3, Projectile.knockBack, Projectile.owner);
+            }
+        }
+
+       
+
+        public override void AI()
+        {
+            Projectile.ai[0]++;
+            Projectile.rotation = Projectile.velocity.ToRotation();
+
+            Lighting.AddLight(Projectile.Center, 0.3f, 0.3f, 1f);
+
+
+            Dust d = Dust.NewDustPerfect(
+             Projectile.Center,
+             DustID.GemRuby,
+             Main.rand.NextVector2Circular(15f, 15f),
+             150,
+             default,
+             1f
+          );
+            Dust d2 = Dust.NewDustPerfect(
+             Projectile.Center,
+             DustID.HeatRay,
+             Main.rand.NextVector2Circular(15f, 15f),
+             150,
+             default,
+             1f
+          );
+            Dust d3 = Dust.NewDustPerfect(
+                        Projectile.Center,
+                        DustID.GemTopaz,
+                        Main.rand.NextVector2Circular(15f, 15f),
+                        150,
+                        default,
+                        1f
+                 );
+            d3.noGravity = true;
+            d.noGravity = true;
+            d2.noGravity = true;
+        }
+    }
+    public class LegoExplosion : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.height = 32;
+            Projectile.width = 32;
+            Projectile.scale = 9f;
+
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.tileCollide = false;
+
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 60;
+
+            Projectile.alpha = 255;
+
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 19;
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            target.AddBuff(BuffID.ShadowFlame, 240);
+
+            if (hit.Crit)
+            {
+                int extraDamage = damageDone * 3;
+                target.SimpleStrikeNPC(extraDamage, 0, false, 0, DamageClass.Melee, false, 0, false);
+
+                SoundEngine.PlaySound(SoundID.AbigailAttack, target.Center);
+                for (int i = 0; i < 10; i++)
+                {
+                    Dust d = Dust.NewDustPerfect(
+                    target.Center,
+                    DustID.GemRuby,
+                    Main.rand.NextVector2Circular(10f, 10f),
+                    150,
+                    default,
+                    1.4f
+
+                 );
+                    Dust d2 = Dust.NewDustPerfect(
+                     target.Center,
+                     DustID.HeatRay,
+                     Main.rand.NextVector2Circular(10f, 10f),
+                     150,
+                     default,
+                     1.4f
+
+                 );
+                    Dust d3 = Dust.NewDustPerfect(
+                        target.Center,
+                        DustID.GemTopaz,
+                        Main.rand.NextVector2Circular(10f, 10f),
+                        150,
+                        default,
+                        1.4f
+
+                 );
+                    d3.noGravity = true;
+                    d.noGravity = true;
+                    d2.noGravity = true;
+
+
+                }
+            }
+        }
+
+
+        public override void AI()
+        {
+
+            Projectile.ai[0]++;
+
+            Lighting.AddLight(Projectile.Center, 0.3f, 0.3f, 1f);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Dust d = Dust.NewDustPerfect(
+                 Projectile.Center,
+                 DustID.GemRuby,
+                 Main.rand.NextVector2Circular(20f, 20f),
+                 150,
+                 default,
+                 3f
+
+              );
+                Dust d2 = Dust.NewDustPerfect(
+                 Projectile.Center,
+                 DustID.HeatRay,
+                 Main.rand.NextVector2Circular(20f, 20f),
+                 150,
+                 default,
+                 3f
+
+              );
+                Dust d3 = Dust.NewDustPerfect(
+                            Projectile.Center,
+                            DustID.GemTopaz,
+                            Main.rand.NextVector2Circular(20f, 20f),
+                            150,
+                            default,
+                            3f
+
+                     );
+                d3.noGravity = true;
+                d.noGravity = true;
+                d2.noGravity = true;
+            }
+        }
+    }
+}
+
