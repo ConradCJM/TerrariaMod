3bcd351590fb35447cac357e36978a4e8748b09c
CJ Mejia <cjconradjoshmejia@gmail.com>
2026-07-01 00:02:18 -0400

    pumpkin projectiles :)

--- Content/Projectiles/Pumpkins/PumpkinProjectile.cs
+++ Content/Projectiles/Pumpkins/PumpkinProjectile.cs
@@ -76,6 +76,55 @@ namespace SomethingCreative.Content.Projectiles.Pumpkins
                         );
 
             }
+
+            Projectile.NewProjectile(
+                Projectile.GetSource_FromThis(),
+                Projectile.Center,
+                Vector2.Zero,
+                ModContent.ProjectileType<PumpkinProjectileExplosion>(),
+                Projectile.damage,
+                Projectile.knockBack,
+                Projectile.owner
+            );
+        }
+    }
+    public class PumpkinProjectileExplosion : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 50;
+            Projectile.height = 50;
+            Projectile.scale = 1f;
+            Projectile.friendly = true;
+            Projectile.tileCollide = false;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.penetrate = -1; 
+            Projectile.timeLeft = 90;
+
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 29;
+
+            Projectile.alpha = 255;
+
+        }
+        public override void AI()
+        {
+            Projectile.ai[0]++;
+            // Create dust effect for the explosion
+
+            
+            if (Projectile.ai[0] % 10 == 0 || Projectile.ai[0] < 3)
+            {
+                Dust d = Dust.NewDustPerfect(
+                    Projectile.Center,
+                    DustID.JungleSpore,
+                    Main.rand.NextVector2Circular(20f, 20f),
+                    150,
+                    new Color(255, 32, 0, 1),
+                    6f
+                );
+                d.noGravity = true;
+            }
         }
     }
 }
