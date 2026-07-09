59b8b2fd50b45c0a5abcfc3ca5f5ee95f92c22a7
CJ Mejia <cjconradjoshmejia@gmail.com>
2026-07-09 00:21:50 -0400

    Create chlorophyte pumpkin and spores. (No sound design yet)

--- Content/Items/Ammo/Pumpkins/ChlorophytePumpkinAmmo.cs
+++ Content/Items/Ammo/Pumpkins/ChlorophytePumpkinAmmo.cs
@@ -8,7 +8,7 @@ using Terraria.ModLoader;
 
 namespace SomethingCreative.Content.Items.Ammo.Pumpkins
 {
-    public class CrystalSlimePumpkinAmmo : ModItem
+    public class ChlorophytePumpkinAmmo : ModItem
     {
         public override void SetDefaults()
         {
@@ -16,25 +16,24 @@ namespace SomethingCreative.Content.Items.Ammo.Pumpkins
             Item.height = 54;
             Item.maxStack = 999;
             Item.scale = 0.8f;
-            Item.rare = ItemRarityID.Pink;
 
             Item.value = Item.buyPrice(0);
-            Item.damage = 20;
+            Item.rare = ItemRarityID.Green;
+            Item.damage = 30;
 
             Item.DamageType = DamageClass.Ranged;
             Item.knockBack = 2f;
             Item.ammo = ModContent.ItemType<PumpkinAmmo>();
-            Item.shoot = ModContent.ProjectileType<CrystalSlimePumpkinProjectile>();
+            Item.shoot = ModContent.ProjectileType<ChlorophytePumpkinProjectile>();
 
         }
 
         public override void AddRecipes()
         {
             Recipe recipe = CreateRecipe(10);
-            recipe.AddIngredient(ItemID.GelBalloon, 5);
-            recipe.AddIngredient(ItemID.CrystalShard, 5);
-            recipe.AddIngredient(ModContent.ItemType<PumpkinAmmo>(), 10);
-            recipe.AddTile(TileID.WorkBenches);
+            recipe.AddIngredient(ItemID.ChlorophyteOre, 10);
+            recipe.AddIngredient(ModContent.ItemType<DirtPumpkinAmmo>(), 10);
+            recipe.AddTile(TileID.AdamantiteForge);
             recipe.Register();
         }
     }
---
+++ Content/Projectiles/Pumpkins/ChlorophytePumpkinProjectile.cs
@@ -0,0 +1,230 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria.Audio;
+
+namespace SomethingCreative.Content.Projectiles.Pumpkins
+{
+    public class ChlorophytePumpkinProjectile : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 54;
+            Projectile.height = 52;
+            Projectile.scale = 0.75f;
+
+            Projectile.friendly = true;
+            Projectile.tileCollide = false;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.penetrate = 1;
+            Projectile.timeLeft = 600;
+
+
+        }
+
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation();
+
+            NPC target = null;
+            float closestDist = 400f;
+
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                NPC npc = Main.npc[i];
+                if (npc.CanBeChasedBy())
+                {
+                    float dist = Vector2.Distance(npc.Center, Projectile.Center);
+                    if (dist < closestDist)
+                    {
+                        closestDist = dist;
+                        target = npc;
+                    }
+                }
+            }
+
+
+            if (target != null)
+            {
+                Vector2 toTarget = target.Center - Projectile.Center;
+                toTarget.Normalize();
+
+
+                Projectile.velocity = Projectile.velocity * 1f +
+                   toTarget * 5f;                //add homing force
+            }
+
+            Dust d = Dust.NewDustPerfect(
+                            Projectile.Center,
+                            DustID.Chlorophyte,
+                            Main.rand.NextVector2Circular(5f, 5f),
+                            150,
+                            default,
+                            2f
+                        );
+            Dust d2 = Dust.NewDustPerfect(
+                            Projectile.Center,
+                            DustID.ChlorophyteWeapon,
+                            Main.rand.NextVector2Circular(5f, 5f),
+                            150,
+                            default,
+                            2f
+                        );
+            d.noGravity = true;
+            d2.noGravity = true;
+
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value;
+
+            Vector2 origin = new Vector2(texture.Width / 2f, texture.Height / 2f);
+
+
+            float alphaMult = (255 - Projectile.alpha) / 255f;
+
+
+            Color drawColor = lightColor * alphaMult;
+
+            Main.EntitySpriteDraw(
+                texture,
+                Projectile.Center - Main.screenPosition,
+                null,
+                drawColor,
+                Projectile.rotation,
+                origin,
+                Projectile.scale,
+                SpriteEffects.None,
+                0
+            );
+
+            return false;
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            int numProjectiles = Main.rand.Next([4,5,6,7,8]); // Number of projectiles to spawn
+            for (int i = 0; i < numProjectiles; i++)
+            {
+                float angle = MathHelper.ToRadians(360f / numProjectiles * i);
+                Vector2 velocity = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle)) * 10f; // Adjust speed as needed
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, velocity, ModContent.ProjectileType<ChloroSpawn>(), Projectile.damage/5, Projectile.knockBack, Projectile.owner);
+            }
+            
+        }
+        public class ChloroSpawn : ModProjectile
+        {
+
+
+            public override void SetDefaults()
+            {
+                Projectile.width = 16;
+                Projectile.height = 32;
+                Projectile.scale = 1f;
+                Projectile.friendly = true;
+                Projectile.tileCollide = false;
+                Projectile.DamageType = DamageClass.Ranged;
+                Projectile.penetrate = -1;
+                Projectile.timeLeft = 300;
+
+                Projectile.usesLocalNPCImmunity = true;
+                Projectile.localNPCHitCooldown = 60;
+
+
+
+            }
+            public override void AI()
+            {
+                Projectile.ai[0]++;
+                Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+
+
+                if (Projectile.ai[0] > 15)
+                {
+                    NPC target = null;
+                    float closestDist = 800f;
+
+                    for (int i = 0; i < Main.maxNPCs; i++)
+                    {
+                        NPC npc = Main.npc[i];
+                        if (npc.CanBeChasedBy())
+                        {
+                            float dist = Vector2.Distance(npc.Center, Projectile.Center);
+                            if (dist < closestDist)
+                            {
+                                closestDist = dist;
+                                target = npc;
+                            }
+                        }
+                    }
+
+
+                    if (target != null)
+                    {
+                        Vector2 toTarget = target.Center - Projectile.Center;
+                        toTarget.Normalize();
+
+
+                        Projectile.velocity = Projectile.velocity * 1f +
+                           toTarget * 10f;                //add homing force
+                    }
+
+                    Dust d = Dust.NewDustPerfect(
+                                    Projectile.Center,
+                                    DustID.Chlorophyte,
+                                    Main.rand.NextVector2Circular(5f, 5f),
+                                    150,
+                                    default,
+                                    2f
+                                );
+                    Dust d2 = Dust.NewDustPerfect(
+                                    Projectile.Center,
+                                    DustID.ChlorophyteWeapon,
+                                    Main.rand.NextVector2Circular(5f, 5f),
+                                    150,
+                                    default,
+                                    2f
+                                );
+                    d.noGravity = true;
+                    d2.noGravity = true;
+                }
+            }
+
+            public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+            {
+
+            }
+
+            public override bool PreDraw(ref Color lightColor)
+            {
+                Texture2D texture = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value;
+
+                Vector2 origin = new Vector2(texture.Width / 2f, texture.Height / 2f);
+
+
+                float alphaMult = (255 - Projectile.alpha) / 255f;
+
+
+                Color drawColor = lightColor * alphaMult;
+
+                Main.EntitySpriteDraw(
+                    texture,
+                    Projectile.Center - Main.screenPosition,
+                    null,
+                    drawColor,
+                    Projectile.rotation,
+                    origin,
+                    Projectile.scale,
+                    SpriteEffects.None,
+                    0
+                );
+
+                return false;
+            }
+        }
+    }
+}
